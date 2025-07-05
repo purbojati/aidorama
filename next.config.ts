9650ef5744f9fcc77f68b5d5449aad5fcf8d102e
Adjie Purbojati <official.purbojati@gmail.com>
2025-07-05 21:26:09 +0700

    Add S3 configuration to next.config.ts for asset hosting

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,12 @@ const nextConfig: NextConfig = {
 				port: '',
 				pathname: '/**',
 			},
+			{
+				protocol: "https",
+				hostname: "s3.aidorama.app",
+				port: "",
+				pathname: "/**",
+			},
 		],
 	},
 	async rewrites() {
